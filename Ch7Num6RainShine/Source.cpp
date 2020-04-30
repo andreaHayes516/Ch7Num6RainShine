c2c39a09ade987bb38f16a5a45ec434278c29e5a
Andrea Hayes <amock516@gmail.com>
2020-04-30 14:47:28 -0700

    Add project files.

---
+++ Ch7Num6RainShine/Source.cpp
@@ -0,0 +1,107 @@
+/************************************************
+** Author: Andrea Hayes
+** Date: April 30, 2020
+** Purpose: Create program to track weather data
+** Input: File, Weather, Day, Month
+** Processing: Report for data file
+** Output: Report for each month and three month period
+           how many days for each weather type. Also 
+           month with most rainy days
+*************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+using namespace std;
+int main() {
+    const int MONTHS = 3, DAYS = 30;
+    char type[MONTHS][DAYS];
+    string monthNames[MONTHS] = { "June", "July", "August" };
+    int count=0,highest, totalS[MONTHS], totalC[MONTHS], totalR[MONTHS];
+    ifstream inputFile;
+
+    inputFile.open("C:/Users/Andrea Hayes/source/repos/Ch7Num6RainShine/RainOrShine.txt"); // Open file
+
+    if (!inputFile) // If file does not open
+    {
+        cout << "Could not open file.";
+        return 1;
+    }
+
+    for (int i = 0; i < MONTHS; i++) {
+        for (int j = 0; j < DAYS; j++) {
+            inputFile >> type[i][j];
+           // cout << i << " " << j << " " << type[i][j] << endl; Display array to make sure it's pulling from file correctly.
+        }
+    }
+
+    inputFile.close();
+
+    // Get rainy days
+    for (int i = 0; i < MONTHS; i++)
+    {
+        totalR[i] = 0;
+        for (int j = 0; j < DAYS; j++)
+        {
+            if (type[i][j] == 'R')
+                totalR[i]++;
+        }
+    }
+    // Get cloudy days
+    for (int i = 0; i < MONTHS; i++)
+    {
+        totalC[i] = 0;
+        for (int j = 0; j < DAYS; j++)
+        {
+            if (type[i][j] == 'C')
+                totalC[i]++;
+        }
+    }
+
+    // Get sunny days
+    for (int i = 0; i < MONTHS; i++)
+    {
+        totalS[i] = 0;
+        for (int j = 0; j < DAYS; j++)
+        {
+            if (type[i][j] == 'S')
+                totalS[i]++;
+        }
+    }
+    // Disply Rainy, Cloudy and Sunny
+    for (int i = 0; i < MONTHS; i++)
+    {
+        cout << monthNames[i] << " " << "Rainy:" << totalR[i] << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < MONTHS; i++)
+    {
+        cout << monthNames[i] << " " << "Cloudy:" << totalC[i] << endl;
+    }
+    
+    cout << endl;
+
+    for (int i = 0; i < MONTHS; i++)
+    {
+        cout << monthNames[i] << " " << "Sunny:" << totalS[i] << endl;
+   }
+
+    // Get month with most rainy days
+    int biggestMonth = 0;
+    highest = totalR[0];
+    for (int i = 0; i < MONTHS; i++)
+    {
+        if (totalR[i] > highest)
+        {
+            highest = totalR[i];
+            biggestMonth = i;
+        }
+    }
+    
+    cout << endl;
+    cout << "Month with most rainy days: " << monthNames[biggestMonth] << endl;
+
+    return 0;
+}
